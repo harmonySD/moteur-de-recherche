17406e44e283c54fff3b4e296b6559907cdcf5f7
Tristan <tristan.jeanne@etu.u-paris.fr>
2023-02-02 15:19:04 +0100

    Début d'implémentation matrice CLI

---
+++ Maain/src/maths/Matrice.java
@@ -0,0 +1,27 @@
+package maths;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implémentation d'une matrice au format CLI.
+ */
+public class Matrice {
+    private List<Float> C;
+    private List<Integer> L;
+    private List<Integer> I;
+
+    public Matrice(){
+        this.C = new ArrayList<>();
+        this.L = new ArrayList<>();
+        this.I = new ArrayList<>();
+    }
+
+    public void insertPage(List<Integer> idList){
+        //Trier la liste
+
+        //C = 1/size.
+        //L = Indice de début de la page.
+        //I = Liste des id d'article trié.
+    }
+}
