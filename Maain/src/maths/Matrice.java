203fb4596c80342b4a597297753418e78d867f02
Tristan <tristan.jeanne@etu.u-paris.fr>
2023-02-09 15:05:38 +0100

    Avancement sur la matrice CLI et Vecteur
    - Algo de multiplaction pas complet
    - Fichier de test vide

--- Maain/src/maths/Matrice.java
+++ Maain/src/maths/Matrice.java
@@ -1,15 +1,26 @@
 package maths;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Implémentation d'une matrice au format CLI.
  */
 public class Matrice {
-    private List<Float> C;
-    private List<Integer> L;
-    private List<Integer> I;
+    /**
+     * Contiens les coefficients de chaque mot (id) dans la matrice.
+     */
+    private final List<Float> C;
+    /**
+     * Liste des indices de début de chaque ligne de la matrice.
+     * L[i] représente le début de la ligne i. Cette ligne se fini à L[i+1]-1.
+     */
+    private final List<Integer> L;
+    /**
+     * Indice de la colonne associée au coefficient C.
+     */
+    private final List<Integer> I;
 
     public Matrice(){
         this.C = new ArrayList<>();
@@ -17,11 +28,35 @@ public class Matrice {
         this.I = new ArrayList<>();
     }
 
-    public void insertPage(List<Integer> idList){
+    /**
+     * Insère les IDs, pointant vers d'autre page, d'une page dans la matrice.
+     * @param pageIdList la liste des IDs pointant vers d'autre page.
+     */
+    public void insertPage(List<Integer> pageIdList){
         //Trier la liste
-
+        Collections.sort(pageIdList);
         //C = 1/size.
+        for(int i = 0; i < pageIdList.size(); i++){
+            this.C.add(1f/pageIdList.size());
+        }
         //L = Indice de début de la page.
+        this.L.add(pageIdList.size() + this.L.get(this.L.size()-1));
         //I = Liste des id d'article trié.
+        this.I.addAll(pageIdList);
+    }
+
+    /**
+     * Multiplie cette matrice par un vecteur.
+     * @param u le vecteur.
+     * @return le vecteur résultant de la multiplication.
+     */
+    public Vecteur multiplyByVector(Vecteur u){
+        //TODO
+        // Il faut aussi transformer tous les 0 en 1/n et aussi prendre en compte epsilon.
+        /*  Pour i de 0 à |L|-1
+                Pour j de L[i] à L[i+1]-1
+                    v[...] += C[j]*u[i]
+         */
+        return new Vecteur();
     }
 }
--- Maain/src/maths/Vecteur.java
+++ Maain/src/maths/Vecteur.java
@@ -1,5 +1,50 @@
 package maths;
 
-public class Vecteur {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Classe représentant un vecteur de Float.
+ */
+public class Vecteur {
+    /**
+     * Liste contenant les valeurs du vecteur.
+     */
+    private List<Float> vecteur;
+
+    public Vecteur(){
+        this.vecteur = new ArrayList<>();
+    }
+
+    /**
+     * Insère une valeur dans le vecteur à l'index donnée.
+     * Si une valeur est déjà présente à l'index donné elle est remplacée
+     * par la valeur passée en paramètre sinon elle est ajoutée à la fin du vecteur.
+     * @param value la valeur à ajouter/modifier dans le vecteur.
+     * @param index la position d'ajout/ modification de la valeur.
+     */
+    public void insertValueAt(float value, int index){
+        if(this.vecteur.size() > index){
+            this.vecteur.set(index, value);
+        }else {
+            this.vecteur.add(index, value);
+        }
+    }
+
+    /**
+     * Insère la valeur à la fin du vecteur.
+     * @param value la valeur à insérer.
+     */
+    public void insertValue(float value){
+        this.vecteur.add(value);
+    }
+
+    /**
+     * Retourne la valeur du vecteur à l'index donné.
+     * @param index l'index dont on veut récupérer la valeur dans le vecteur.
+     * @return la valeur à l'index donné.
+     */
+    public float getValueAt(int index){
+        return this.vecteur.get(index);
+    }
 }
